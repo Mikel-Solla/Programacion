08e0400fc36b0beb6e56b5715f52ec1e2d7eb8bc
Mikel <mikel.solla@ikasle.egibide.org>
2019-10-30 14:01:36 +0100

    Seguir eje9

--- EjerciciosProgramacion/Eje9_Practica3/src/eje9_practica3/Eje9_Practica3.java
+++ EjerciciosProgramacion/Eje9_Practica3/src/eje9_practica3/Eje9_Practica3.java
@@ -13,17 +13,59 @@ import javax.swing.JOptionPane;
  */
 public class Eje9_Practica3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    
     public static void main(String[] args) {
         String nombre=JOptionPane.showInputDialog("tu nombre");
+        Pedido();
         Factura(nombre);
     }
     
     public static String Factura(String nombre){
-        JOptionPane.showMessageDialog(null, "NOMBRE:"+nombre);
+        JOptionPane.showMessageDialog(null, "NOMBRE: "+nombre+"\nLINEAS DE DETALLES: ");
         return nombre;
+    }
+    
+    public static void Pedido(){
+        String resp="";
+        
+        do{
+            try{
+                Producto();
+                Cantidad();
+                resp=JOptionPane.showInputDialog("¿Quieres seguir?");
+                resp=resp.toUpperCase();
+        }
+            catch(NullPointerException e){
+                JOptionPane.showMessageDialog(null,"Boton cancelar y x desactivados");
+            }
+        }
+        while(resp.equals("SI"));
+        
+    }
+    
+    public static void Producto(){
+        int precio;
+        double IVA;
+        String producto=JOptionPane.showInputDialog("¿Que producto quieres?\n1-Papas\n2-Papas fritas\n3-Papas al horno");
+        producto=producto.toUpperCase();
+        switch(producto){
+            case "PAPAS":
+                precio=500;
+                IVA=(precio*0.04);
+                break;
+            case "PAPAS FRITAS":
+                precio=1000;
+                IVA=(precio*0.1);
+                break;
+            case "PAPAS AL HORNO":
+                precio=1500;
+                IVA=(precio*0.21);
+        }
+    }
+    
+    public static void Cantidad(){
+        int cantidad=Integer.parseInt(JOptionPane.showInputDialog("¿Cuanto quieres?"));        
     }
     
 }
